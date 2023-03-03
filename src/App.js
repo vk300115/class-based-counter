ed537edefd4b733a2cd70071c91c6579b79c1c2c
Vladislav Kalinovskij <119266924+vk300115@users.noreply.github.com>
2023-03-03 01:02:03 +0000

    add basic component and layout

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Banner } from "./components/Banner";
+import { Counter } from "./components/Counter";
+
+export const App = () => {
+  return <main>
+    <Banner />
+
+    <Counter />
+  </main>;
+};
