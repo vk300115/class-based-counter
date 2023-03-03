60ed99e935c5447fcee3b1d3ef6b23033ec0ba4a
Vladislav Kalinovskij <119266924+vk300115@users.noreply.github.com>
2023-03-03 01:35:28 +0000

    complete styled counter

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
 export const Banner = () => {
-  return <section className="banner">
+  return <section className="banner border m-2">
     <h1>React Counter</h1>
     <h2>Please use the buttons to interact with your counter</h2>
   </section>;
